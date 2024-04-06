fe6b9ffba0fd6c4c0a45177b25953d588f40b5a2
wiggins <you@example.com>
2024-04-06 13:54:00 -0400

    w00t

---
+++ westbonnerschools2304/downloads.sh
@@ -0,0 +1,3 @@
+#curl https://www.youtube.com/@pittsylvaniacountyschools4099/streams | tr '{}' '\n' | grep -e 'videoId' | cut -d, -f1 | cut -d: -f2 | tr '"[]' '\n' | sort -bfdu | grep -v -e '^true$' | grep -v '^$' | awk '{print "yt-dlp --write-auto-sub --skip-download "$0}' >! yt.sh
+curl https://www.youtube.com/@westbonnerschools2304/videos | tr '{}' '\n' | grep -e 'videoId' | cut -d, -f1 | cut -d: -f2 | tr '"[]' '\n' | sort -bfdu | grep -v -e '^true$' | grep -v '^$' | awk '{print "yt-dlp --write-auto-sub --skip-download "$0}' >! yt.sh
+source yt.sh
